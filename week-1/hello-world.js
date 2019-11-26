6d353b2cfe51d29a3c518acc8cdffa21e408251f
bmulhern2 <bmulhern09@gmail.com>
2019-11-26 10:47:18 -0500

    week-1

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,11 @@
-console.log("Hello World, my name is Brendan");
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Professor Krasso
+; Date: 26 November 2019
+; Modified By: Brendan Mulhern
+; Description: This program prints out the text "Hello World, my name is Brendan".
+;=======
+
+
+console.log("Hello World, my name is Brendan"); // Prints out the term "Hello World, my name is Brendan".
