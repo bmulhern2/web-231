63d4a80fff49da0e793a08f0e54589c7110d461f
bmulhern2 <bmulhern09@gmail.com>
2019-11-26 17:31:30 -0500

    commit

---
+++ week-1/mulhern-Types_Values_And_Variables/mulhern-Types_Values_And_Variables.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title: Assignment 1.5
+; Author: Professor Krasso
+; Date: 26 November 2019
+; Modified By: Brendan Mulhern
+; Description:
+;========
+
+class Employee {
+    constructor(lastName, address, payRate, hireDate);
+    lastName = 
+}
