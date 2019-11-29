6177dd041bb66be90218b6e52b2afbb0e78d29b4
bmulhern2 <bmulhern09@gmail.com>
2019-11-29 11:56:18 -0500

    twoErrorsFixed

---
+++ week-1/JessCruse-Discussion1.1/JessCruse-Discussion1.1.js
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title: Discussion 1.1
+; Author: Jess Cruse
+; Date: 28 November 2019
+; Modified By: Brendan Mulhern
+; Description: This is a simple JavaScript
+; program using values and variables with
+; at least two errors.
+;===========================================
+*/
+
+var now = new Date();
+
+var empFirst = "Pat";
+var empLast = "Brown";
+
+//output
+console.log("Employee " + empFirst + " " + empLast + " added on " + now);
