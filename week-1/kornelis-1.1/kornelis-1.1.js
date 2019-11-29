ebb3b353aeb28233817326a6436c332f5c9d1fb8
bmulhern2 <bmulhern09@gmail.com>
2019-11-29 12:05:57 -0500

    first commit

---
+++ week-1/kornelis-1.1/kornelis-1.1.js
@@ -0,0 +1,25 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Izabella Kornelis
+; Date: 27 November 2019
+; Modified By: Izabella Kornelis
+; Description: A simple JavaScript program using values and variables with at least two errors.
+;===========================================
+*/
+
+//program start
+
+//variable declaration
+var x = 10;
+
+//creating function
+function multiply(x) {
+    x *= x;
+    return x;
+};
+
+//output
+console.log(multiply(x));
+
+//end program
