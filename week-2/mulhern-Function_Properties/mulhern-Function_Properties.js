9695b881a0f4eea1754c8146a77271093c76c5fe
bmulhern2 <bmulhern09@gmail.com>
2019-12-03 05:52:54 -0500

    12.3.19

--- week-2/mulhern-Function_Properties/mulhern-Function_Properties.js
+++ week-2/mulhern-Function_Properties/mulhern-Function_Properties.js
@@ -1,3 +1,12 @@
-var myName = function() { return myName.brendan};
-var myName.brendan = function() {};
-console.log(myName());
+/*
+============================================
+; Title: Assignment 2.3 - Function Properties
+; Author: Brendan Mulhern
+; Date: 3 December 2019
+; Modified By: Brendan Mulhern
+; Description: This program exemplifies properties used in Javascript Functions.
+;========
+*/
+var myName = function() { return myName.brendan}; // Declares Function myName and allows this function to equal myName.brendan
+myName.brendan = "Brendan"; // Allows for myName.brendan to equal "Brendan"
+console.log(myName()); // Prints the output of the function myName which equals myName.brendan which equals "Brendan"
