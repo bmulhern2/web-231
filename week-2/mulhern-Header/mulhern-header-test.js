eba5c6f60b1e5e4da2b703d0002457c359a60635
bmulhern2 <bmulhern09@gmail.com>
2019-12-07 10:49:20 -0500

    commit

--- week-2/mulhern-Header/mulhern-header-test.js
+++ week-2/mulhern-Header/mulhern-header-test.js
@@ -1,3 +1,12 @@
+/*
+============================================
+; Title: Exercise 2.2
+; Author: Professor Krasso
+; Date: 7 December 2019
+; Modified By: Brendan Mulhern
+; Description: Displays a formatted header
+;===========================================
+*/
 const header = require('./mulhern-Header.js');
 
 console.log(header.display("Brendan", "Mulhern", "Exercise 2.2"));
