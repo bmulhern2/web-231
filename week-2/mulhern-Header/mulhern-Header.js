32641dae26a4f5fa3acd74254c5ee6ffe9a5961a
bmulhern2 <bmulhern09@gmail.com>
2019-12-07 10:47:16 -0500

    12.7.19

---
+++ week-2/mulhern-Header/mulhern-Header.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title: Exercise 2.2
+; Author: Professor Krasso
+; Date: 7 December 2019
+; Modified By: Brendan Mulhern
+; Description: Displays a formatted header
+;===========================================
+*/
+exports.display = function (firstName, lastName, assignment) {
+let output = firstName + " " + lastName + " " + assignment;
+return output;
+};
---
+++ week-2/mulhern-Header/mulhern-header-test.js
@@ -0,0 +1,3 @@
+const header = require('./mulhern-Header.js');
+
+console.log(header.display("Brendan", "Mulhern", "Exercise 2.2"));
