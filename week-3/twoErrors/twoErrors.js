5e27013039c12dad87731b176b3fd13f6cdedaa3
bmulhern2 <bmulhern09@gmail.com>
2019-12-10 14:27:42 -0500

    commit

---
+++ week-3/twoErrors/twoErrors.js
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title: Discussion Board 3.1
+; Author: Brendan Mulhern
+; Date: 10 Decemeber 2019
+; Modified By: Brendan Mulhern
+; Description: This is a loop program which contains two errors.
+;===========================================
+*/
+
+function isItTwenty(n) {
+  if (n === 20 ) {
+  console.log("It's not 20");
+} else {
+  console.log("It's 20");
+}};
+
+isItTwenty(20);
+isItTwenty(19);
