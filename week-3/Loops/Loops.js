46540343f14d981e7ed5e63e097aade751c5e0ee
bmulhern2 <bmulhern09@gmail.com>
2019-12-15 22:51:45 -0500

    commit

--- week-3/Loops/Loops.js
+++ week-3/Loops/Loops.js
@@ -19,7 +19,7 @@ function match(num1, num2) {
 
 // Defines the function logMismatch which states if the numbers are not equal or not.
 function logMismatch(num1, num2) {
-    console.log(num1 + " does not equal " + num2);
+    console.log(num1 + " does equal " + num2);
     };
 
 // Defines the function logMatch which states if the two values are equal.
@@ -28,18 +28,14 @@ function logMatch(num1, num2) {
 };
 
 // This is where the for loop is; I commented it out because it wouldn't run becuase it was infitite.
-//for (let i = 0; i < 10; i++) {};
-// Here is where I defined num1
-if (i = 1) {
+for (let i = 0; i < 10; i++) {
   var num1 = Math.floor(Math.random());
-// Here is where I defined num3
-} else if (i = 3) {
   var num3 = Math.floor(Math.random());
 };
 // Here is where the functions match; logmatch are run.
 if (match(num1, num3) === "True") {
   logMatch(num1, num3);
 // Here is where match and logMismatch are run.
-} else if (match(num1, num3) === "False") {
+} else {
   logMismatch(num1, num3);
 };
