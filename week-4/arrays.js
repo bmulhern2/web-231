8091f0b9908d745d02a30d971d8e57d9739276bf
bmulhern2 <bmulhern09@gmail.com>
2019-12-19 12:27:07 -0500

    commit

---
+++ week-4/arrays.js
@@ -0,0 +1,16 @@
+/*
+============================================
+; Title: Exercise 4.2 - Arrays
+; Author: Brendan Mulhern
+; Date: 19 December 2019
+; Modified By: Brendan Mulhern
+; Description: This is a program that displays the qualities of arrays.
+;===========================================
+*/
+var fruit = ["Pear", "Apple", "Orange", "Blackberry", "Blueberry"];
+
+function getFruit(array) {
+  console.log(array);
+};
+
+getFruit(fruit);
