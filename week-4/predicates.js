c78200c0859cd6ecd08e783aba06b66e8ec8d6a1
bmulhern2 <bmulhern09@gmail.com>
2019-12-19 13:05:50 -0500

    commit

--- week-4/predicates.js
+++ week-4/predicates.js
@@ -4,27 +4,30 @@
 ; Author: Brendan Mulhern
 ; Date: 19 December 2019
 ; Modified By: Brendan Mulhern
-; Description:
+; Description: This is a program of which prints the contents of an array; filters an array and checks to see if two values are the same.
 ;===========================================
 */
 // Declares array of five states
 var states = ["New Hampshire", "Maine", "Colorado", "Vermont", "Nebraska"];
-
+var selected = "Colorado";
 // Declares function getState with two sting parameters.
-function getState(state1, state2) {
+function getState(state, selected) {
+// For loop testing to see if the states matcch the selected value.
+for(i = 0; i < states.length; i++) {
 // An if statement checking if the two strings match.
-if (state1 === state2) {
+if (state[i] === selected) {
   console.log("True");
-}};
+}}};
 
 // Uses filter to call the getState function
 var filteredStates = states.filter(getState);
 // Displays content of orginal array
 console.log("Original Array");
+// For loop defining orig
 console.log(states);
 // Displays content of updates array
 console.log("Updated Array");
-console.log(filteredStates);
+console.log(filteredStates[0]);
 // Displays selected value
 console.log("Selected Value");
-console.log(filteredStates);
+console.log(selected);
