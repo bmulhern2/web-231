6628a07c26680edda73d818b773394aa9a6a9414
bmulhern2 <bmulhern09@gmail.com>
2019-12-21 13:28:01 -0500

    commit

---
+++ week-4/stacy-discussion4-edit.js
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title: Assignment 4.1
+; Author: Clayton Stacy
+; Date: 19 December 2019
+; Modified By:
+; Description: Arrays discussion, find two errors
+============================================
+*/
+
+// Import header and write it to the console.
+var header = require('../week-2/Stacy-header.js');
+console.log(header.display('Clayton', 'Stacy', '3.2 Pattern Matching Functions'));
+
+//Declare the array teams with teams
+// Array was defined in an errorly fashion..
+const teams = ["Steelers", "Cowboys", "Eagles", "Ravens", "Giants", "Browns"];
+
+//Iterate through the array and sort them into divisions
+// There were not semi-colons after the brackets.
+teams.forEach(teams => {
+    if (team === 'Browns' || team === 'Steelers' || team === 'Ravens') {
+        //Log the AFC North teams using template literals
+        console.log(`The ${ team } play in the AFC North`)
+    } else {
+        console.log(`The ${ team } play in the NFC East`)
+    };
+});
