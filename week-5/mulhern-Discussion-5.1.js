abd599cfbcce9d9b8e702e73a217d42c7a97cf47
bmulhern2 <bmulhern09@gmail.com>
2020-01-09 14:37:07 -0500

    commit

--- week-5/mulhern-Discussion-5.1.js
+++ week-5/mulhern-Discussion-5.1.js
@@ -7,3 +7,32 @@
 ; Description: This is a keyed collection program with two intentional errors in it.
 ;===========================================
 */
+
+// Declares a new map of buildings
+var soccerPosition = new Map();
+
+// Sets maps to be of type position "Goalie" with the corresponding function "save"
+const position1 = soccerPosition.set('Goalie', 'Save');
+
+// Sets maps to be of type position "Defense" with the corresponding function "defend"
+const position2 = soccerPosition.set('Defense', 'Defend');
+
+// Sets maps to be of type position "Midfield" with the corresponding function "pass"
+const position3 = soccerPosition.set('Midfield', 'Pass');
+
+// Sets maps to be of type position "Striker" with the corresponding function "score"
+const position4 = soccerPosition.set('Striker', 'Score');
+
+// Sets maps to be of type position "Coach" with the corresponding function "coach"
+const position5 = soccerPosition.set('Coach', 'Coach');
+
+// Defines a function of printPositions
+function printPositions(key, value) {
+// Defines a for loop that prints positions
+
+for (i=0; i <= 4; i++) {
+  console.log(key + ' gets to ' + value);
+}};
+
+// Prints Poistions
+printPositions();
