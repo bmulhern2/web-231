2f71342031ed50bbcde94e286753a842a15f9dfd
bmulhern2 <bmulhern09@gmail.com>
2020-01-09 14:16:00 -0500

    commit

---
+++ week-5/mulhern-Discussion-5.1.js
@@ -0,0 +1,9 @@
+/*
+============================================
+; Title: Discussion Board 5.1
+; Author: Brendan Mulhern
+; Date: 9 January 2020
+; Modified By: Brendan Mulhern
+; Description: This is a keyed collection program with two intentional errors in it.
+;===========================================
+*/
