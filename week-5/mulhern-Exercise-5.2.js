550f701fb1a9821a8e3149b6a593a6e8bcc2c544
bmulhern2 <bmulhern09@gmail.com>
2020-01-11 12:13:37 -0500

    commit

---
+++ week-5/mulhern-Exercise-5.2.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title: Discussion Board 5.1
+; Author: Brendan Mulhern
+; Date: 9 January 2020
+; Modified By: Brendan Mulhern
+; Description: This is a keyed collection program with two intentional errors in it.
+;===========================================
+*/
+
+var foods = ["Pasta", "Lasagna", "Tortellini", "Pizza", "Calzone"];
+
+foods.forEach(function(food){ console.log(food)});
