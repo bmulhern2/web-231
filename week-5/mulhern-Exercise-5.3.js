2d2532159c3b7b8ea9f89e3710788a05e27b96fc
bmulhern2 <bmulhern09@gmail.com>
2020-01-11 12:23:15 -0500

    commit

--- week-5/mulhern-Exercise-5.3.js
+++ week-5/mulhern-Exercise-5.3.js
@@ -4,6 +4,34 @@
 ; Author: Brendan Mulhern
 ; Date: 9 January 2020
 ; Modified By: Brendan Mulhern
-; Description: This is a keyed collection program with two intentional errors in it.
+; Description: This program defines an array of composer objects and prints out the array.
 ;===========================================
 */
+var realAndFakePeople = [
+  {
+    firstName: 'Herbie',
+    lastName: 'Hancock',
+  },
+
+  {
+    firstName: 'Joel',
+    lastName: 'McGuinnes',
+  },
+
+  {
+    firstName: 'Chris',
+    lastName: 'Christopher',
+  },
+
+  {
+    firstName: 'Brendan',
+    lastName: 'Eich',
+  },
+
+  {
+    firstName: 'Mike',
+    lastName: 'Mikelson',
+  },
+];
+
+realAndFakePeople.forEach(function(name){console.log(name.firstName + " " + name.lastName)});
