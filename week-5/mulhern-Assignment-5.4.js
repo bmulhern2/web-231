3866f6734aa4ffb2329e90b71bdf828fbbf78503
bmulhern2 <bmulhern09@gmail.com>
2020-01-11 12:46:06 -0500

    commit

--- week-5/mulhern-Assignment-5.4.js
+++ week-5/mulhern-Assignment-5.4.js
@@ -7,3 +7,50 @@
 ; Description: This program defines an array by genre and prints out the array.
 ;===========================================
 */
+
+// Defines array of famous music artists
+var famousMusicArtists = [
+  {
+    firstName: "Joel",
+    lastName: "Zimmerman",
+    genre: "EDM",
+    rating: "5",
+  },
+  {
+    firstName: "Chris",
+    lastName: "Comstock",
+    genre: "EDM",
+    rating: "5",
+  },
+  {
+    firstName: "Herbie",
+    lastName: "Hancock",
+    genre: "Jazz",
+    rating: "4",
+  },
+  {
+    firstName: "Matthew",
+    lastName: "Samuels",
+    genre: "Hip-Hop",
+    rating: "4",
+  },
+  {
+    firstName: "Isabelle",
+    lastName: "Rezazadeh",
+    genre: "Electronic",
+    rating: "3",
+  },
+];
+// Prints the artist by rating
+console.log("By Rating:");
+famousMusicArtists.map(function(name){console.log(name.rating + " " + name.firstName + " " + name.lastName)});
+
+// Prints the artist by genre
+console.log(" ");
+console.log("By Genre:");
+famousMusicArtists.map(function(name){console.log(name.genre + " " + name.firstName + " " + name.lastName)});
+
+// Prints the whole contents of the array
+console.log(" ");
+console.log("Complete Array:");
+famousMusicArtists.forEach(function(name){console.log(name.firstName + " " + name.lastName + " " + name.genre + " " + name.rating)});
