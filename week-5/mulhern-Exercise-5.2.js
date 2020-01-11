6662c1d44d944e1ecb01f244c0ca72ddac489554
bmulhern2 <bmulhern09@gmail.com>
2020-01-11 12:15:22 -0500

    commit

--- week-5/mulhern-Exercise-5.2.js
+++ week-5/mulhern-Exercise-5.2.js
@@ -4,10 +4,12 @@
 ; Author: Brendan Mulhern
 ; Date: 9 January 2020
 ; Modified By: Brendan Mulhern
-; Description: This is a keyed collection program with two intentional errors in it.
+; Description: This is a program that defiens an array then prints out the conents of the array.
 ;===========================================
 */
 
+// Defines array foods
 var foods = ["Pasta", "Lasagna", "Tortellini", "Pizza", "Calzone"];
 
+// Prints out the array
 foods.forEach(function(food){ console.log(food)});
