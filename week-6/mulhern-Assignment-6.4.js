2067a33d49326036bbf0d1d88f88410d93a8d2dd
bmulhern2 <bmulhern09@gmail.com>
2020-01-13 12:41:53 -0500

    first commit

---
+++ week-6/mulhern-Assignment-6.4.js
@@ -0,0 +1,9 @@
+/*
+============================================
+; Title: Assignment 6.4
+; Author: Brendan Mulhern
+; Date: 13 January 2020
+; Modified By: Brendan Mulhern
+; Description: This is a program that covers Nested Object Literals.
+;===========================================
+*/
