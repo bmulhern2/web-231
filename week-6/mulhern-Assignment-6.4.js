b7c98ecf15274ea252d41d31041874f6af4d6089
bmulhern2 <bmulhern09@gmail.com>
2020-01-13 13:00:21 -0500

    first commit

--- week-6/mulhern-Assignment-6.4.js
+++ week-6/mulhern-Assignment-6.4.js
@@ -7,3 +7,21 @@
 ; Description: This is a program that covers Nested Object Literals.
 ;===========================================
 */
+var ticket = {
+  id: "2000",
+  name: "Parked In Wrong Spot",
+  dateCreated: "1/13/20",
+  priority: "No",
+  person: {
+    id: "2020",
+    firstName: "Billy",
+    lastName: "Jones",
+    jobTitle: "Software Enginner 2"
+  }
+}
+
+console.log("Brendan Mulhern");
+console.log("Assignment 6.4");
+console.log("1/13/20");
+console.log("");
+console.log("Ticket " + ticket.id + " was created on " + ticket.dateCreated + " and assigned to employee " + ticket.person.firstName + " " + ticket.person.lastName + " (" + ticket.person.jobTitle + ").");
--- week-6/mulhern-Exercise-6.3.js
+++ week-6/mulhern-Exercise-6.3.js
@@ -7,6 +7,7 @@
 ; Description: This is a program that covers Object Literals
 ;===========================================
 */
+// Defines array
 var ticketSystem = [
   {
     id: "1",
@@ -15,4 +16,5 @@ var ticketSystem = [
   }
 ];
 
+// Prints out array
 ticketSystem.forEach(function(ticketSystem){console.log(ticketSystem.id + " " + ticketSystem.name + " " + ticketSystem.requestor)});
